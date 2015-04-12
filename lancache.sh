687da57e7645a2432d97421bd7dc7515301f5548
Marcin Pawlowski <github@marcin.io>
2015-04-12 02:11:10 -0700

    cleanup start script

--- lancache.sh
+++ lancache.sh
@@ -2,11 +2,12 @@
 
 IP4=`/sbin/ifconfig eth1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'`
 
-if hash docker 2>/dev/null; 
+if ! hash docker 2>/dev/null; 
 then
-	docker pull mpawlowski/lancache
-	docker run -e HOST_ADDRESS=`echo $IP4` -it mpawlowski/lancache
-else
 	echo "docker not installed"
-	exit 1	
+	exit 1
 fi
+
+docker pull mpawlowski/lancache
+docker run -e HOST_ADDRESS=`echo $IP4` -it mpawlowski/lancache
+
